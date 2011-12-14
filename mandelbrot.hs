968b7f373d0bd46918535cc20e8580acca127525
davorb <davor@davor.se>
2011-12-14 10:47:04 +0100

    Lowered the resolution and cleaned some stuff up

--- mandelbrot.hs
+++ mandelbrot.hs
@@ -1,26 +1,26 @@
--- mandelbrot.hs
--- (c) 2011 Davor Babic <davor@davor.se>
+-- mandelbrot.hs - (c) 2011 Davor Babic <davor@davor.se>
 import Complex
-                        
+
 z :: (Num a2, Num a) => a -> a2 -> a2
 z 0 _ = 0
 z k c = (z (k-1) c)*(z (k-1) c) + c
 
-isPartOfSet a = test a 1                
-  where test a k | k > 19 = True
+isPartOfSet a = test a 1
+  where test a k | k > iterations = True
                  | realPart (z k a) > 2  = False
                  | otherwise             = test a (k+1)
 
-init_x = (-2.1)
-init_y = (-1.4)
-step_x = 0.025
-step_y = 0.07
-
-render x y | x >= 0.8 = '\n' : render init_x (y+step_y)
-           | y >= 1.4 = []
-           | isPartOfSet (x :+ y)  = '.' : render (x+step_x) y
+render x y | x >= max_x = '\n' : render init_x (y+step_y)
+           | y >= max_y = []
+           | isPartOfSet (x :+ y)  = '*' : render (x+step_x) y
            | otherwise             = ' ' : render (x+step_x) y
 
-mandel = putStrLn $ render init_x init_y
+main = putStrLn $ render init_x init_y
 
-main = mandel
+init_x = (-2.1)
+init_y = (-1.4)
+max_x  = 0.8
+max_y  = 1.4
+step_x = 0.04
+step_y = 0.07
+iterations = 30
